8be355baecf3a9a56737b39fb44728af410e62af
Han Kruiger <hankruiger@gmail.com>
2018-03-13 21:09:47 +0100

    Hello deeplearn.

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 import "scss/style.scss";
 import "img/favicon.png";
+import * as dl from "deeplearn"; // If not loading the script as a global
+
+const a = dl.tensor1d([1, 2, 3]);
+const b = dl.scalar(2);
+
+const result = a.add(b); // a is not modified, result is a new tensor
+result.data().then(data => console.log(data)); // Float32Array([3, 4, 5]
+
+// Alternatively you can use a blocking call to get the data.
+// However this might slow your program down if called repeatedly.
+console.log(result.dataSync()); // Float32Array([3, 4, 5]

5e1425880c67ec7552693d32df345c6211d8d04e
Han Kruiger <hankruiger@gmail.com>
2018-06-10 15:36:53 +0200

    Better verification.

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,33 +1,19 @@
 import * as tf from "@tensorflow/tfjs";
+import {Promise} from "es6-promise";
 import "img/favicon.png";
 import "scss/style.scss";
 
-// Create some elements that will display the result.
-const operand1Element = document.createElement("span");
-const operatorElement = document.createElement("span");
-const operand2Element = document.createElement("span");
-const equalsElement = document.createElement("span");
-const resultElement = document.createElement("span");
-
-document.body.appendChild(operand1Element);
-document.body.appendChild(operatorElement);
-document.body.appendChild(operand2Element);
-document.body.appendChild(equalsElement);
-document.body.appendChild(resultElement);
-
-operatorElement.innerHTML = "+";
-equalsElement.innerHTML = "=";
+document.body.innerHTML = "Computing...";
 
 const a = tf.scalar(1);
-a.data().then((data) => {
-    operand1Element.innerHTML = String(data[0]);
-});
 const b = tf.scalar(1);
-b.data().then((data) => {
-    operand2Element.innerHTML = String(data[0]);
-});
-
 const result = a.add(b);
-result.data().then((data) => {
-    resultElement.innerHTML = String(Math.round(data[0]));
-});
+
+Promise.all([a.data(), b.data(), result.data()])
+    .then((data) => {
+        const rounded = data.map(((x) => Math.round(x[0])));
+        document.body.innerHTML = `${rounded[0]} + ${rounded[1]} = ${rounded[2]}`;
+    })
+    .catch((error) => {
+        document.body.innerHTML = `Something went wrong: ${error}`;
+    });
